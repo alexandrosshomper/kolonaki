792dc52119457ac78c76a667e546a667ca41995b
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2025-10-29 15:22:24 +0100

    Remove middleware.js file, which contained session update logic and request path matching configuration.

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request) {
+export async function proxy(request) {
   // update user's auth session
   return await updateSession(request);
 }
